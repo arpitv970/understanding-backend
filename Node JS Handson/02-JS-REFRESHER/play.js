7f932251bc47173869b2f0cb28e3a5d9e472c24a
Arpit Verma <arpitv970@gmail.com>
2022-04-11 20:19:57 +0530

    added hands-on for js basics

--- Node JS Handson/02-JS-REFRESHER/play.js
+++ Node JS Handson/02-JS-REFRESHER/play.js
@@ -1,1 +1,17 @@
- 
+var name = "Madara Uchiha"; // String
+var age = 30; // Integer
+var EMS = true; // Boolean
+
+function summerizeShinobi(shinobiName, shinobiAge, KekkeiGenkai) {
+  // parameters inside brackets have local-scope only
+  return (
+    "The Shinobi is " +
+    shinobiName +
+    ", age is " +
+    shinobiAge +
+    " & possess Kekkei Genkai " +
+    KekkeiGenkai
+  );
+}
+
+console.log(summerizeShinobi(name, age, EMS)); // this way we can directly prints of function's return value(s)
