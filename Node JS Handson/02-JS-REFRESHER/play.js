df5a3e35c6c818722ac75c67e69540c28a1e9dfd
Arpit Verma <arpitv970@gmail.com>
2022-04-12 16:12:21 +0530

    Arrow functions

--- Node JS Handson/02-JS-REFRESHER/play.js
+++ Node JS Handson/02-JS-REFRESHER/play.js
@@ -2,7 +2,7 @@ var name = "Madara Uchiha"; // String
 var age = 30; // Integer
 var EMS = true; // Boolean
 
-function summerizeShinobi(shinobiName, shinobiAge, KekkeiGenkai) {
+const summerizeShinobi = (shinobiName, shinobiAge, KekkeiGenkai) => {
   // parameters inside brackets have local-scope only
   return (
     "The Shinobi is " +
@@ -12,6 +12,6 @@ function summerizeShinobi(shinobiName, shinobiAge, KekkeiGenkai) {
     " & possess Kekkei Genkai " +
     KekkeiGenkai
   );
-}
+};
 
 console.log(summerizeShinobi(name, age, EMS)); // this way we can directly prints of function's return value(s)
