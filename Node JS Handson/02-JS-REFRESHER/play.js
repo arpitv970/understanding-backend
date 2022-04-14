2a64ae5af4ec086ca9628cf410d918cd27753b26
Arpit Verma <arpitv970@gmail.com>
2022-04-15 01:14:02 +0530

    added hands on for Spread & Rest Operator

--- Node JS Handson/02-JS-REFRESHER/play.js
+++ Node JS Handson/02-JS-REFRESHER/play.js
@@ -1,12 +1,28 @@
-const arr = ['Yoriichi', 106, {name: 'Sakura', trash: true}];
+const arr = ["Yoriichi", 106, { name: "Sakura", trash: true }];
 
 // iterating through an array
 // for (let element of arr) {
 //   console.log(element);
 // }
 
-// meathods 
+// meathods
 // console.log(arr.map(element => `element: ${element}`));
 // console.log(arr);
-arr.push({Chakra: 'Wind'});
-console.log(arr);
+arr.push({ Chakra: "Wind" });
+// const cpyArr = [arr]; // this would create a nested array instead of copy of it
+const cpyArr = [...arr]; // yikes!! now it would create a true copy of array
+// console.log(cpyArr);
+
+// this would return only 3 arguments in output
+// const displayArgs = (arg1, arg2, arg3) => {
+//     return [arg1, arg2, arg3];
+// }
+
+// console.log(displayArgs(1, 2, 3, 4));   // In this case, only first three agruments would be returned
+
+// this would return multiple arguments in output
+const displayArgs = (...args) => {
+    return args;
+}
+
+console.log(displayArgs(1, 2, 3, 4));   // In this case, all agruments would be returned
