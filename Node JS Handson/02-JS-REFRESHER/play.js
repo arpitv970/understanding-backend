0e29de68eea539774aedf7886acd6cf4dd27e93c
Arpit Verma <arpitv970@gmail.com>
2022-04-20 00:50:56 +0530

    Async & promise handson

--- Node JS Handson/02-JS-REFRESHER/play.js
+++ Node JS Handson/02-JS-REFRESHER/play.js
@@ -1,48 +1,19 @@
-// const arr = ["Yoriichi", 106, { name: "Sakura", trash: true }];
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        // argument "resolve": it would complete the Promise successfully
+        // argument "reject": it would reject i.e. throwing an error 
+        setTimeout(() => {
+            resolve('Data Fetched!!');
+        }, 1500);
+    });
+    return promise; // this is sync code, thus promise is immediately returned after setTimeout() expires
+};
 
-// iterating through an array
-// for (let element of arr) {
-//   console.log(element);
-// }
+setTimeout(() => {
+    console.log("Timeout!!")
+    fetchData().then(test => {
+        console.log(test);
+    });
+}, 2000);   // this is async code as it do not execute instantly
 
-// meathods
-// console.log(arr.map(element => `element: ${element}`));
-// console.log(arr);
-// arr.push({ Chakra: "Wind" });
-// const cpyArr = [arr]; // this would create a nested array instead of copy of it
-// const cpyArr = [...arr]; // yikes!! now it would create a true copy of array
-// console.log(cpyArr);
-
-// this would return only 3 arguments in output
-// const displayArgs = (arg1, arg2, arg3) => {
-//     return [arg1, arg2, arg3];
-// }
-
-// console.log(displayArgs(1, 2, 3, 4));   // In this case, only first three agruments would be returned
-
-// this would return multiple arguments in output
-// const displayArgs = (...args) => {
-//     return args;
-// }
-
-// console.log(displayArgs(1, 2, 3, 4));   // In this case, all agruments would be returned
-
-
-const person = {
-    name: 'Levi Ackerman', 
-    age: 30,
-}
-
-const printName = ({ name, age }) => {
-    console.log(`Name: ${name}, Age: ${age}`);
-}
-
-const { name, age } = person;
-
-// console.log(name, age);
-
-// printName(person);
-
-const arr = ["Tsunade", 106, { name: "Sakura", trash: true }];
-const [character, size] = arr;
-console.log(character, size);
+console.log("Hello");
