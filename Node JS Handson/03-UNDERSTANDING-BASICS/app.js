ae233619c8236d118aa5847abea16dd601e8fe54
Arpit Verma <arpitv970@gmail.com>
2022-04-22 23:32:39 +0530

    redirecting requests

--- Node JS Handson/03-UNDERSTANDING-BASICS/app.js
+++ Node JS Handson/03-UNDERSTANDING-BASICS/app.js
@@ -1,14 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;    // parsing 'url'
+    const method = req.method;  //parsing 'method'
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Akatsuki Recruitment</title><head>');
+        res.write('<body><form action="message" method="POST"><input type="text" name="joining"><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();    
+    }
+
+    // In the following if ladder we want the url set back to '/' & to create a new file to store the user's message
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Test Text');
+        res.statusCode = 302; // status code for re-direction
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Node.js Tutorial</title><head>');
     res.write('<body><h1>Tatakae!!</h1></body>');
     res.write('</html>');
     res.end();
-    
+
 });
 
 server.listen(3000);
