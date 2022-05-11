40b8270c2ec51647ab9d15f312c9b3fa1c4a799e
Arpit Verma <arpitv970@gmail.com>
2022-05-12 00:09:38 +0530

    serving html file

--- Node JS Handson/03-UNDERSTANDING-BASICS/routes/shop.js
+++ Node JS Handson/03-UNDERSTANDING-BASICS/routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');   // utitlity by express
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>This is created using Express meathod</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));   // this would yeild an address that would be compatible for both linux and windows OS
 });
 
 module.exports = router;
