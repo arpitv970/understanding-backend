d264a2984f99196c1424178f4c6821b8fa0e6cf6
Arpit Verma <arpitv970@gmail.com>
2022-05-12 19:38:26 +0530

    major changes updated

--- Node JS Handson/03-UNDERSTANDING-BASICS/routes/shop.js
+++ Node JS Handson/03-UNDERSTANDING-BASICS/routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');   // utitlity by express
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));   // this would yeild an address that would be compatible for both linux and windows OS
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));   // this would yeild an address that would be compatible for both linux and windows OS
 });
 
 module.exports = router;
