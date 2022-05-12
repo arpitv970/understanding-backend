bb7d1e4c8523f1fe9ca23395ba93ae3ebe69772b
Arpit Verma <arpitv970@gmail.com>
2022-05-12 19:32:05 +0530

    There are 2 major updates in project
    - Serving HTML file using `path.join()`
    - Serving files/folder statically using express

--- Node JS Handson/03-UNDERSTANDING-BASICS/app.js
+++ Node JS Handson/03-UNDERSTANDING-BASICS/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes); // '/admin' is used to filter url
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
