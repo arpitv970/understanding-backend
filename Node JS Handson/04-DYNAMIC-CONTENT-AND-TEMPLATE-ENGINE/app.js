014b08370e4cf66f0c742aa2bcce7612c15f8802
Arpit Verma <arpitv970@gmail.com>
2022-05-16 23:39:01 +0530

    minor bug fixed

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes); // '/admin' is used to filter url
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.render('shop');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
