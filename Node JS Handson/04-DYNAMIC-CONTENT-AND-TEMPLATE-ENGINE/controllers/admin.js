c058576adb1647d2a76bde7a30b8b9d6cdcbcd4b
Arpit Verma <arpitv970@gmail.com>
2022-06-12 17:59:50 +0530

    modified the way to post added product

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/admin.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/admin.js
@@ -1,25 +1,29 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render("admin/add-product", {
-      pageTitle: "Add Product",
-      path: "/admin/add-product",
-    });
-  };
-  
-  exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save(); // this saves the title in the array created in models
-    res.redirect("/");
-  };
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+};
 
-  exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-      console.log(products);
-      res.render("admin/products", {
-        prods: products,
-        pageTitle: "Admin Products",
-        path: "/admin/products",
-      });
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save(); // this saves the title in the array created in models
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    console.log(products);
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
-  };
+  });
+};
