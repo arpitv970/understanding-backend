afe6552ff9e618be3114211fb20576b77e8d87bf
Arpit Verma <arpitv970@gmail.com>
2022-06-12 18:02:15 +0530

    feature added for order list

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -29,6 +29,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders"
+  });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
