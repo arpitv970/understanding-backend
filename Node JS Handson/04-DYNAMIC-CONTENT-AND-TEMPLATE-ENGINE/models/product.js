5b74c8ba79a35540b09d6dbd9b6b7131aa6269bb
Arpit Verma <arpitv970@gmail.com>
2022-06-12 18:11:03 +0530

    constructor passed by all kind of info related to a particular order

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
@@ -19,8 +19,11 @@ const getProductsFromFile = (cb) => {
 
 module.exports = class Product {
   // passing title through constructor
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   // this is a meathod to store
