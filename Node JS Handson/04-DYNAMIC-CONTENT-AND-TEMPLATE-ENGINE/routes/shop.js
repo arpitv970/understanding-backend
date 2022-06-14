01f308d5d53d6904932bfbd360f3f1dd4a3bfa5c
Arpit Verma <arpitv970@gmail.com>
2022-06-14 20:35:07 +0530

    dynamic expressjs router added

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/routes/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/routes/shop.js
@@ -10,6 +10,10 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
+// the ":" is to tell express that the route path is dynamic
+// if you have dynamic path similar to exact path like "/products/delete" & "/products/:productID", always put dynamic one at the last
+router.get("/products/:productId", shopController.getProduct);
+
 router.get("/cart", shopController.getCart);
 
 router.get("/orders", shopController.getOrders);
