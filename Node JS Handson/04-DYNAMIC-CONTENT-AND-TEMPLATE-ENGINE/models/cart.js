d6ef56adc425e9fc11a722ff2a4161f27debe0b1
Arpit Verma <arpitv970@gmail.com>
2022-06-15 22:13:43 +0530

    a cart is being created

---
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/cart.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  // As cart will be always there, we just need to somehow manage products
+  static addProduct(id, productPrice) {
+    // Fetch previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      // Add new product / increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct }; // using rest operatore
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct]; // using spreding operator
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice; // add '+' before productPrice makes it an integer for addition
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
