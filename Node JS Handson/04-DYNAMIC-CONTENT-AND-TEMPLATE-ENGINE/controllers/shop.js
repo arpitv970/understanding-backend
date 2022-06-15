df21c97e3a6c04d1c026c878ce3dd418baa29871
Arpit Verma <arpitv970@gmail.com>
2022-06-15 22:04:20 +0530

    controller added for postCart

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -14,11 +15,15 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   // console.log("The product id is: ", prodId);
-  Product.findById(prodId, product => {
-    console.log(product);
+  Product.findById(prodId, (product) => {
+    // console.log(product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
   });
-  res.redirect('/');
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -34,20 +39,28 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
-    pageTitle: "Your Cart"
+    pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
-    pageTitle: "Your Orders"
+    pageTitle: "Your Orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
-    pageTitle: "Checkout"
+    pageTitle: "Checkout",
   });
 };
