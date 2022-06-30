848a040245f68358f1073576d0d91b4e04dc9ee7
Arpit Verma <arpitv970@gmail.com>
2022-06-30 21:07:06 +0530

    used Sequelize for defining models

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
@@ -1,33 +1,28 @@
-const db = require('../utils/database'); // importing db pool
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../utils/database');
 
-module.exports = class Product {
-    // passing title through constructor
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false
     }
+});
 
-    // this is a meathod to store
-    save() {
-        return db.execute(
-            'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-            [this.title, this.price, this.imageUrl, this.description]
-        );
-    }
 
-    static deleteById(id) {}
-
-    // fetchAll fetches all the products, thus we use static to use it just for one
-    static fetchAll() {
-        return db.execute('SELECT *  FROM products');
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    }
-};
+module.exports = Product;
