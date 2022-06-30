014b01a18df30bfdae220b6b2d1216917afe2fe9
Arpit Verma <arpitv970@gmail.com>
2022-06-30 17:14:10 +0530

    added functionality to insert data into MySQL DB

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
@@ -1,32 +1,31 @@
-const db = require('../utils/database');  // importing db pool
+const db = require('../utils/database'); // importing db pool
 
-const Cart = require("./cart");
+const Cart = require('./cart');
 
 module.exports = class Product {
-  // passing title through constructor
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+    // passing title through constructor
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
 
-  // this is a meathod to store
-  save() {
-    
-  }
+    // this is a meathod to store
+    save() {
+        return db.execute(
+            'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+            [this.title, this.price, this.imageUrl, this.description]
+        );
+    }
 
-  static deleteById(id) {
-    
-  }
+    static deleteById(id) {}
 
-  // fetchAll fetches all the products, thus we use static to use it just for one
-  static fetchAll() {
-    return db.execute('SELECT *  FROM products');
-  }
+    // fetchAll fetches all the products, thus we use static to use it just for one
+    static fetchAll() {
+        return db.execute('SELECT *  FROM products');
+    }
 
-  static findById(id) {
-    
-  }
+    static findById(id) {}
 };
