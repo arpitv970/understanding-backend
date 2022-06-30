76de52566be6670cb464aded10a574d9dd1d1c84
Arpit Verma <arpitv970@gmail.com>
2022-06-30 17:30:47 +0530

    added feature to fetch one product from MySQL DB

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -19,14 +19,18 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     // console.log("The product id is: ", prodId);
-    Product.findById(prodId, (product) => {
-        // console.log(product);
-        res.render('shop/product-detail', {
-            product: product,
-            pageTitle: product.title,
-            path: '/products',
+    Product.findById(prodId)
+        .then(([product]) => {
+            // console.log(product);
+            res.render('shop/product-detail', {
+                product: product[0],
+                pageTitle: product.title,
+                path: '/products',
+            });
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    });
 };
 
 exports.getIndex = (req, res, next) => {
--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/product.js
@@ -27,5 +27,7 @@ module.exports = class Product {
         return db.execute('SELECT *  FROM products');
     }
 
-    static findById(id) {}
+    static findById(id) {
+        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+    }
 };
