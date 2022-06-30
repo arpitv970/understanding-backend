b9476c00a81e401d2d8d1046d7cf072d536f4300
Arpit Verma <arpitv970@gmail.com>
2022-06-30 18:40:22 +0530

    connected to mysql db using sequelize

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/utils/database.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/utils/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'admin',
+const sequelize = new Sequelize('node-complete', 'root', 'admin', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
