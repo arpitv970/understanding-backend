288a08056656a3561539bc0dcf5f7bd78af78828
Arpit Verma <arpitv970@gmail.com>
2022-07-02 10:40:39 +0530

    created model schema for dummy user

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 // importing required controller(s)
 const getController = require('./controllers/error');
 const sequelize = require('./utils/database'); // this would be the pool of connections
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express(); // running express as function
 
@@ -19,6 +21,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 // utilizing the imported route(s)
 app.use('/admin', adminRoutes); // '/admin' is used to filter url
 app.use(shopRoutes);
@@ -26,10 +39,27 @@ app.use(shopRoutes);
 // utilizing the imported controller(s)
 app.use(getController.get404);
 
+Product.belongsTo(User, {
+    constraint: true,
+    onDelete: 'CASCADE',
+});
+User.hasMany(Product);
+
 sequelize
+    // .sync({ force: true })
     .sync()
     .then((result) => {
         // console.log(result);
+        return User.findByPk(1);
+    })
+    .then((user) => {
+        if (!user) {
+            return User.create({ name: 'Arpit', email: 'test@gmail.com' });
+        }
+        return user;
+    })
+    .then((user) => {
+        // console.log(user);
         app.listen(3000);
     })
     .catch((err) => {
