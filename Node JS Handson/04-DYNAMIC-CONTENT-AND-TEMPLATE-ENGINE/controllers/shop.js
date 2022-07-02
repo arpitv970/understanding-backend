a8d0809c17a38c26349c7db0bc508f9dc576c56e
Arpit Verma <arpitv970@gmail.com>
2022-07-02 18:17:13 +0530

    deleting item from cart functionality added

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -106,9 +106,21 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    const productPrice = req.body.prodPrice;
-    Cart.deleteProduct(prodId, productPrice);
-    res.redirect('/cart');
+    req.user
+        .getCart()
+        .then((cart) => {
+            return cart.getProducts({ where: { id: prodId } });
+        })
+        .then((products) => {
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then((result) => {
+            res.redirect('/cart');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 exports.getOrders = (req, res, next) => {
