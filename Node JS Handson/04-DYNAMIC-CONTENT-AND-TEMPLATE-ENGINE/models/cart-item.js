b2b9120890fa15eb234f952224f31b6a833b3991
Arpit Verma <arpitv970@gmail.com>
2022-07-02 17:26:08 +0530

    Fetched data to cart

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/cart-item.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/cart-item.js
@@ -9,7 +9,7 @@ const CartItem = sequelize.define('cartItem', {
         allowNull: false,
         primaryKey: true,
     },
-    quantity: Sequelize.STRING,
+    quantity: Sequelize.INTEGER,
 });
 
 module.exports = CartItem;
