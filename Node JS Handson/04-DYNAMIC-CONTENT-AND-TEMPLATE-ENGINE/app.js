fcc6e7d7d7227aaf3fb7bc51ae44f7eb738dd4d8
Arpit Verma <arpitv970@gmail.com>
2022-07-04 23:38:04 +0530

    intalled & basic mongodb setup done

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
@@ -5,13 +5,7 @@ const bodyParser = require('body-parser');
 
 // importing required controller(s)
 const getController = require('./controllers/error');
-const sequelize = require('./utils/database'); // this would be the pool of connections
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./utils/database');
 
 const app = express(); // running express as function
 
@@ -19,64 +13,31 @@ app.set('view engine', 'ejs'); // this would set template engine to ejs
 app.set('views', 'views'); // this can be omitted here, as it is useful when we uses name other than views
 
 // importing route(s)
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then((user) => {
-            req.user = user;
-            next();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    // User.findByPk(1)
+    //     .then((user) => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
 });
 
 // utilizing the imported route(s)
-app.use('/admin', adminRoutes); // '/admin' is used to filter url
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes); // '/admin' is used to filter url
+// app.use(shopRoutes);
 
 // utilizing the imported controller(s)
 app.use(getController.get404);
 
-Product.belongsTo(User, {
-    constraint: true,
-    onDelete: 'CASCADE',
+mongoConnect((client) => {
+    console.log(client);
+    app.listen(3000);
 });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-
-sequelize
-    // .sync({ force: true })
-    .sync()
-    .then((result) => {
-        // console.log(result);
-        return User.findByPk(1);
-    })
-    .then((user) => {
-        if (!user) {
-            return User.create({ name: 'Arpit', email: 'test@gmail.com' });
-        }
-        return user;
-    })
-    .then((user) => {
-        // console.log(user);
-        return user.createCart();
-    })
-    .then((cart) => {
-        app.listen(3000);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/utils/database.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/utils/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'admin', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://arpit:admin@cluster0.numu8.mongodb.net/?retryWrites=true&w=majority'
+    )
+        .then((client) => {
+            console.log('Connected!!');
+            callback(client);
+        })
+        .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
