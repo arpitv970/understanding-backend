9818369f255fa37aa63587d8afb725eb163b66c2
Arpit Verma <arpitv970@gmail.com>
2022-07-06 11:06:22 +0530

    updated user model

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/user.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/user.js
@@ -4,13 +4,30 @@ const getDb = require('../utils/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
     save() {
         const db = getDb();
         db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        const cartProduct = this.cart.items.findIndex((cp) => {
+            return cb._id == product._id;
+        });
+
+        const upddateCart = { items: [{ ...product, quantity: 1 }] };
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: upddateCart } }
+            );
     }
 
     static findById(userId) {
