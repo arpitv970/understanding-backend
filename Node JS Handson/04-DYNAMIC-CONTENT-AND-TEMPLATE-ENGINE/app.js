b85548c096d55a079f3a9a4d278faa2dd1ee157f
Arpit Verma <arpitv970@gmail.com>
2022-07-07 16:44:16 +0530

    Refactored for mongoose
    - fetching all products from db
    - getting all products in admin
    - updating product

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/app.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // importing required controller(s)
 const getController = require('./controllers/error');
-const mongoConnect = require('./utils/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express(); // running express as function
 
@@ -20,16 +20,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('62c4696244884fe398dd7ff5')
-        .then((user) => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+// app.use((req, res, next) => {
+//     User.findById('62c4696244884fe398dd7ff5')
+//         .then((user) => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
 
 // utilizing the imported route(s)
 app.use('/admin', adminRoutes); // '/admin' is used to filter url
@@ -38,6 +38,13 @@ app.use(shopRoutes);
 // utilizing the imported controller(s)
 app.use(getController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-});
+mongoose
+    .connect(
+        'mongodb+srv://arpit:admin@cluster0.numu8.mongodb.net/shop?retryWrites=true&w=majority'
+    )
+    .then((result) => {
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/admin.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/admin.js
@@ -13,14 +13,12 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(
-        title,
-        price,
-        description,
-        imageUrl,
-        null,
-        req.user._id
-    );
+    const product = new Product({
+        title: title,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,
+    });
     product
         .save()
         .then((result) => {
@@ -66,16 +64,15 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
 
-    const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        prodId
-    );
+    Product.findById(prodId)
+        .then((product) => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
 
-    product
-        .save()
+            return product.save();
+        })
         .then((result) => {
             console.log('Updated Product!!');
             res.redirect('/admin/products');
@@ -86,7 +83,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then((products) => {
             // console.log(products);
             res.render('admin/products', {
