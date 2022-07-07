e2e33d90f9afa1c11d5c08e6d9909d74965d5538
Arpit Verma <arpitv970@gmail.com>
2022-07-07 22:30:00 +0530

    added clear cart feature after order is placed

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -112,6 +112,9 @@ exports.postOrder = (req, res, next) => {
             return order.save();
         })
         .then((result) => {
+            return req.user.clearCart();
+        })
+        .then(() => {
             res.redirect('/orders');
         })
         .catch((err) => {
--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/user.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/user.js
@@ -62,6 +62,11 @@ userSchema.methods.removeFromCart = function (productId) {
     return this.save();
 };
 
+userSchema.methods.clearCart = function () {
+    this.cart = {items: []};
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 // const mongodb = require('mongodb');
 // const getDb = require('../utils/database').getDb;
