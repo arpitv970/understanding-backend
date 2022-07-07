d8bf01b6fb2df314ba5aadcde7a2845dcc6e53fc
Arpit Verma <arpitv970@gmail.com>
2022-07-07 22:52:12 +0530

    fetched orders

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -123,8 +123,7 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user
-        .returnOrders()
+    Order.find({ 'user.userId': req.user._id })
         .then((orders) => {
             res.render('shop/orders', {
                 path: '/orders',
