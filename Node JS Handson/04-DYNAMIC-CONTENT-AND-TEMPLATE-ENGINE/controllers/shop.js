f2be194cb63d6d4e564ac26a996c89712d357ae4
Arpit Verma <arpitv970@gmail.com>
2022-07-07 22:21:54 +0530

    orders stored using mongoose

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
     Product.find()
@@ -92,9 +93,24 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
     req.user
-        .createOrder()
+        .populate('cart.items.productId')
+        .then((user) => {
+            const products = user.cart.items.map((i) => {
+                return {
+                    quantity: i.quantity,
+                    product: { ...i.productId._doc },
+                };
+            });
+            const order = new Order({
+                products: products,
+                user: {
+                    name: req.user.name,
+                    userId: req.user,
+                },
+            });
+            return order.save();
+        })
         .then((result) => {
             res.redirect('/orders');
         })
--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/order.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/models/order.js
@@ -1,15 +1,31 @@
-const Sequelize = require('sequelize');
+const mongoose = require('mongoose');
 
-const sequelize = require('../utils/database');
+const { Schema } = mongoose;
 
-const Order = sequelize.define('order', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
+const orderSchema = new Schema({
+    products: [
+        {
+            product: {
+                type: Object,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    user: {
+        name: {
+            type: String,
+            required: true,
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
     },
-    quantity: Sequelize.INTEGER,
 });
 
-module.exports = Order;
+module.exports = mongoose.model('Order', orderSchema);
