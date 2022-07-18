bc875eee2dcc4139acd262cd133d4bb0eb3811bf
Arpit Verma <arpitv970@gmail.com>
2022-07-18 22:22:55 +0530

    setting redirect request after logging-in followed by saving the session

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
@@ -16,7 +16,10 @@ exports.postLogin = (req, res, next) => {
         .then((user) => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            res.redirect('/');
+            req.session.save((err) => {
+                console.log(err);
+                res.redirect('/');
+            });
         })
         .catch((err) => {
             console.log(err);
