6507457af11a67f1c5a2c208d189fc5faf173999
Arpit Verma <arpitv970@gmail.com>
2022-08-06 22:47:36 +0530

    made basic user login flow

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
@@ -21,14 +21,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('62c6ce9cc73fef6de8e93910')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
         .then((user) => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save((err) => {
-                console.log(err);
-                res.redirect('/');
-            });
+            if (!user) {
+                return res.redirect('/login');
+            }
+            bcrypt
+                .compare(password, user.password)
+                .then((match) => {
+                    if (match) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save((err) => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
         })
         .catch((err) => {
             console.log(err);
