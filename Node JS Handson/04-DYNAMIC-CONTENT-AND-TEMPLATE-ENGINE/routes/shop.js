6530ec1fbfcff2a8268e49cd79ced40eacf0c03c
Arpit Verma <arpitv970@gmail.com>
2022-08-07 22:41:45 +0530

    Protected routes using middleware

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/routes/shop.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/routes/shop.js
@@ -3,6 +3,7 @@ const path = require('path'); // utitlity by express
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/isAuth');
 
 const router = express.Router();
 
@@ -14,14 +15,14 @@ router.get('/products', shopController.getProducts);
 // if you have dynamic path similar to exact path like "/products/delete" & "/products/:productID", always put dynamic one at the last
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.post('/create-order', shopController.postOrder);
+router.post('/create-order', isAuth, shopController.postOrder);
 
-router.get('/orders', shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
