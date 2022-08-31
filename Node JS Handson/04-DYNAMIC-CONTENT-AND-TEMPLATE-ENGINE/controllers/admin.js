1e03884e9023051f0ca830c15289faa9fe8ae69e
Arpit Verma <arpitv970@gmail.com>
2022-08-31 22:33:40 +0530

    Added more protection to the post reqs of Admin

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/admin.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/admin.js
@@ -70,16 +70,18 @@ exports.postEditProduct = (req, res, next) => {
 
     Product.findById(prodId)
         .then((product) => {
+            if (product.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/');
+            }
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDesc;
             product.imageUrl = updatedImageUrl;
 
-            return product.save();
-        })
-        .then((result) => {
-            console.log('Updated Product!!');
-            res.redirect('/admin/products');
+            return product.save().then((result) => {
+                console.log('Updated Product!!');
+                res.redirect('/admin/products');
+            });
         })
         .catch((err) => {
             console.log(err);
@@ -87,7 +89,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({ userId: req.user._id })
         // .select('title price -_id')
         // .populate('userId', 'name')
         .then((products) => {
@@ -105,7 +107,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndDelete(prodId)
+    Product.deleteOne({ _id: prodId, userId: req.user._id })
         .then((result) => {
             console.log('Product deleted successfully!');
             res.redirect('/admin/products');
