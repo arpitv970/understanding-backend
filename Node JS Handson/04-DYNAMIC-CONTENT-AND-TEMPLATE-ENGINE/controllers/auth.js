c3ef4c8b3ea3f33609c99c56809b593a30398996
Arpit Verma <arpitv970@gmail.com>
2022-09-13 21:15:36 +0530

    added validator at login page

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
@@ -22,6 +22,8 @@ exports.getLogin = (req, res, next) => {
         path: '/login',
         pageTitle: 'Login',
         errorMessage: message,
+        oldInput: { email: '', password: '' },
+        validationErrors: [],
     });
 };
 
@@ -56,13 +58,20 @@ exports.postLogin = (req, res, next) => {
             path: '/login',
             pageTitle: 'Login',
             errorMessage: errors.array()[0].msg,
+            oldInput: { email: email, password: password },
+            validationErrors: [],
         });
     }
     User.findOne({ email: email })
         .then((user) => {
             if (!user) {
-                req.flash('error', 'Invalid email or password');
-                return res.redirect('/login');
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password',
+                    oldInput: { email: email, password: password },
+                    validationErrors: [],
+                });
             }
             bcrypt
                 .compare(password, user.password)
@@ -75,8 +84,13 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
-                    req.flash('error', 'Invalid email or password');
-                    res.redirect('/login');
+                    return res.status(422).render('auth/login', {
+                        path: '/login',
+                        pageTitle: 'Login',
+                        errorMessage: 'Invalid email or password',
+                        oldInput: { email: email, password: password },
+                        validationErrors: [],
+                    });
                 })
                 .catch((err) => {
                     req.flash('error', 'Invalid email or password');
