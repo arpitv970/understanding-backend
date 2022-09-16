6b72e0bd15e491efd9a76978406c6c745ed7dbde
Arpit Verma <arpitv970@gmail.com>
2022-09-16 23:41:07 +0530

    Revert "added validator at login page"
    This reverts commit c3ef4c8b3ea3f33609c99c56809b593a30398996.

--- Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
+++ Node JS Handson/04-DYNAMIC-CONTENT-AND-TEMPLATE-ENGINE/controllers/auth.js
@@ -22,8 +22,6 @@ exports.getLogin = (req, res, next) => {
         path: '/login',
         pageTitle: 'Login',
         errorMessage: message,
-        oldInput: { email: '', password: '' },
-        validationErrors: [],
     });
 };
 
@@ -58,20 +56,13 @@ exports.postLogin = (req, res, next) => {
             path: '/login',
             pageTitle: 'Login',
             errorMessage: errors.array()[0].msg,
-            oldInput: { email: email, password: password },
-            validationErrors: [],
         });
     }
     User.findOne({ email: email })
         .then((user) => {
             if (!user) {
-                return res.status(422).render('auth/login', {
-                    path: '/login',
-                    pageTitle: 'Login',
-                    errorMessage: 'Invalid email or password',
-                    oldInput: { email: email, password: password },
-                    validationErrors: [],
-                });
+                req.flash('error', 'Invalid email or password');
+                return res.redirect('/login');
             }
             bcrypt
                 .compare(password, user.password)
@@ -84,13 +75,8 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
-                    return res.status(422).render('auth/login', {
-                        path: '/login',
-                        pageTitle: 'Login',
-                        errorMessage: 'Invalid email or password',
-                        oldInput: { email: email, password: password },
-                        validationErrors: [],
-                    });
+                    req.flash('error', 'Invalid email or password');
+                    res.redirect('/login');
                 })
                 .catch((err) => {
                     req.flash('error', 'Invalid email or password');
